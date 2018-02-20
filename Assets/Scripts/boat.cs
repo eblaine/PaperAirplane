061b8f1a60ad42dc5c2812097833512c3af1f043
Ellen Blaine <eblaine@stanford.edu>
2018-02-20 10:56:52 -0800

    water forces experiment

--- Assets/Scripts/boat.cs
+++ Assets/Scripts/boat.cs
@@ -6,21 +6,25 @@ public class boat : MonoBehaviour {
 
 	public float turnSpeed = 5000f;
 	public float accelSpeed = 5000f;
+	public Vector3 waterForce;
 
 	private Rigidbody rbody;
 
 	// Use this for initialization
 	void Start () {
 		rbody = GetComponent<Rigidbody>();
+		waterForce = new Vector3 (0f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
+//		float v = 1f;
 
 		rbody.AddTorque (0f, h * turnSpeed * Time.deltaTime, 0f);
 		rbody.AddForce (transform.forward * v * accelSpeed * Time.deltaTime);
+		rbody.AddForce (waterForce * Time.deltaTime);
 
 	}
 }
---
+++ Assets/vortex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vortex : MonoBehaviour {
+	private boat boatScript;
+	public float forceMultiplier = 100f;
+	// Use this for initialization
+	void Start () {
+		boatScript = GameObject.Find ("Boat").GetComponent<boat> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = boatScript.transform.position;
+		Vector3 newForce = new Vector3(-currentPosition.x, 0f, -currentPosition.z);
+		boatScript.waterForce = forceMultiplier * newForce;
+//		boat.waterForce = new Vector3(100f, 100f, 100f);
+	}
+}
